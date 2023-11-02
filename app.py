3260e98b8748d0b49229249e44a0357649eafe61
PolyLvst <zeerofive05@gmail.com>
2023-11-02 15:25:58 +0700

    Added cache, loading icon interface

--- app.py
+++ app.py
@@ -1,28 +1,78 @@
 from flask import Flask,render_template,jsonify
+import mysql.connector
+from dotenv import load_dotenv
 import os
+import re
 
 app = Flask(__name__)
+
+load_dotenv()
+HOST_DB = os.environ.get("HOST_DB")
+USER_DB = os.environ.get("USER_DB")
+PASS_DB = os.environ.get("PASS_DB")
+NAME_DB = os.environ.get("NAME_DB")
+db_config = {
+    "host": HOST_DB,
+    "user": USER_DB,
+    "password": PASS_DB,
+    "database": NAME_DB
+}
+
+def connect_db():
+    try:
+        conn = mysql.connector.connect(**db_config)
+    except Exception as e:
+        print(f'{e} \n --- Error --- ')
+        exit()
+    cursor = conn.cursor()
+    return cursor,conn
+
+def _get_cache_users(curr):
+    sql = "SELECT name,full_name,class_id FROM users"
+    curr.execute(sql)
+    data = curr.fetchall()
+    return data
+
+def _get_cache_class(curr):
+    sql = "SELECT id,class_name FROM classes"
+    curr.execute(sql)
+    data = curr.fetchall()
+    return data
 
 @app.route("/",methods=["GET"])
 def main():
     return render_template("index.html")
 
 @app.route("/api/getid/<id>",methods=["GET"])
-def getName(id):
-    dir_path = "static/assets/student-pictures"
-    file_name = id
-    file_path = os.path.join(dir_path,file_name)
-    if os.path.isfile(file_path):
-        file_extension = os.path.splitext(file_path)[1]
-        print(file_path,file_extension)
-    else:
-        print("Not found")
+def get_name(id):
+    # Ini akan berbentuk tuple (Name,Full_Name,id_class)
+    try:
+        results:tuple = [t_stu for t_stu in cache if t_stu[0] == id][0]
+    except IndexError as e:
+        print(f"Id not found - {e}")
+        return jsonify({"msg":"failed, no student with that id"})
+    print(results)
+    class_stu_unformat = cache_class[results[2]-1][1]
+    class_stu = re.sub(r'(?i)kelas', '', class_stu_unformat)
+    print(class_stu)
     data = {
-        "data":{"name":"Test",
-                "class":"12"},
+        "data":{"name":results[1],
+                "class":class_stu,
+                "img_path":"",
+                "status":"terabsen"},
         "msg":"success"
         }
     return jsonify(data)
 
+@app.route("/api/create",methods=["POST"])
+def create_stu():
+    pass
+
 if __name__ == "__main__":
+    cursor,conn = connect_db()
+    cache:list = _get_cache_users(cursor)
+    cache_class:list = _get_cache_class(cursor)
+    # print(cache)
     app.run("localhost",5000,True)
+    cursor.close()
+    conn.close()
--- static/myjs/myjs.js
+++ static/myjs/myjs.js
@@ -1,6 +1,9 @@
+var successDing = "../static/assets/ding.mp3";
+let idleTime = 4 //Seconds
+
 function loadingAnim() {
     const loadingDots = $("#text-menunggu");
-  
+
     function animateLoadingDots() {
         let dots = 1;
         function updateDots() {
@@ -10,7 +13,7 @@ function loadingAnim() {
         updateDots();
         const intervalId = setInterval(updateDots, 500);// Change the dots every 500ms
         // Stop the animation after a certain duration (e.g., 10000ms or 10 seconds)
-        setTimeout(function() {
+        setTimeout(function () {
             clearInterval(intervalId);
             loadingDots.text("Menunggu ...");
         }, 10000);
@@ -18,35 +21,63 @@ function loadingAnim() {
     animateLoadingDots();
 };
 
-function loadStudentPicture(id){
+function loadStudentPicture(id) {
     const pictureBox = $("#picture-box");
     let tempHtml = `<img src="../static/assets/student-pictures/${id}.jpg" alt="" id="picture-student" class="profile-icon">`;
     pictureBox.empty();
     pictureBox.append(tempHtml);
 };
 
-function getNameById(id){
-    let url = "/api/getid/"+id;
+function getNameById(id) {
+    let url = "/api/getid/" + id;
     $.ajax({
-        "type":"GET",
-        "url":url,
-        "data":{},
-        success: function(response){
+        "type": "GET",
+        "url": url,
+        "data": {},
+        success: function (response) {
             let data = response["data"];
             let name = data["name"];
             let classStu = data["class"];
+
             const pictureArea = $("#menunggu-scan-text");
+            const welcomeSection = $("#welcome-section");
+
             pictureArea.empty()
+            welcomeSection.empty()
             loadStudentPicture(id);
+
+            let tempHtml = `
+            <h5>Selamat datang ${name}<h5>
+            <p>Kelas ${classStu}<p>
+            `;
+            welcomeSection.append(tempHtml);
+            playSound(successDing);
         }
     })
-}
+};
 
-function detectTextInput(){
+function replenishWaitingMessage() {
+    const pictureBox = $("#picture-box");
+    const pictureArea = $("#menunggu-scan-text");
+    const welcomeSection = $("#welcome-section");
+    let tempHtml = `
+    <h5>Scan QR</h5>
+    <p id="text-menunggu">Menunggu .</p>
+    <div class="spinner-border spinner-border-sm" role="status">
+        <span class="visually-hidden">Loading...</span>
+    </div>`;
+    welcomeSection.empty();
+    pictureArea.empty();
+    pictureBox.empty();
+    pictureBox.append(`<img src="../static/assets/person-bounding-box.svg" alt="" id="picture-student" class="profile-icon">`);
+    pictureArea.append(tempHtml);
+};
+
+function detectTextInput() {
     const inputElement = $("#text-input-scan");
     const statusElement = $("#status");
 
-    inputElement.on("input", function() {
+    inputElement.on("input", function () {
         let inputStr = inputElement.val().trim();
         if (inputStr !== "" && inputStr.length >= 4) {
             statusElement.text("Text has been filled.");
@@ -55,4 +86,28 @@ function detectTextInput(){
             statusElement.text("");
         }
     });
-}
+};
+
+function detectIdle() {
+    var currSeconds = 0;
+    let idleInterval = setInterval(timerIncrement, 1000);
+    $(this).mousemove(resetTimer);
+    $(this).keypress(resetTimer);
+    function resetTimer() {
+        currSeconds = 0;
+    }
+    function timerIncrement() {
+        if (currSeconds == idleTime) {
+            // console.log('No interaction detected after .. seconds');
+            replenishWaitingMessage();
+            loadingAnim();
+        }
+        currSeconds = currSeconds + 1;
+    }
+};
+
+function playSound(src) {
+    // Create an Audio object
+    var audio = new Audio(src);
+    audio.play();
+};
