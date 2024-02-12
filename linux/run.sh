224c1556f4f68d6f40a25932417356f0bb26d0f4
PolyLvst <zeerofive05@gmail.com>
2024-02-12 11:19:03 +0700

    upgrade requirements, fix missing logs folder

--- linux/run.sh
+++ linux/run.sh
@@ -5,6 +5,16 @@ script_directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $script_directory
 cd ..
+folder_path="./logs"
+
+if [ ! -d "$folder_path" ]; then
+    # If the folder does not exist, create it
+    mkdir -p "$folder_path"
+    echo "Folder logs created successfully."
+else
+    # If the folder already exists, print a message
+    echo "Logger ..."
+fi
 
 echo $(date)>>./logs/logs_cron.txt
 web_server_py="python -m gunicorn -w 1 -b 0.0.0.0:5000 app:app"
--- linux/upload_auto.sh
+++ linux/upload_auto.sh
@@ -6,6 +6,16 @@ script_directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $script_directory
 cd ..
 
+folder_path="./logs"
+
+if [ ! -d "$folder_path" ]; then
+    # If the folder does not exist, create it
+    mkdir -p "$folder_path"
+    echo "Folder logs created successfully."
+else
+    # If the folder already exists, print a message
+    echo "Logger ..."
+fi
 echo $(date)>>./logs/logs_cron.txt
 lazy_attend="./lazy_attend.py"
 while true
